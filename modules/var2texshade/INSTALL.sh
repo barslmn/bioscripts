5f679ce84b10b5ddd84529dbf5d5cfb9d5c653da
bar <bar@E15G3.home>
2022-11-10 16:34:42 +0300

    changed dependency

--- modules/var2texshade/INSTALL.sh
+++ modules/var2texshade/INSTALL.sh
@@ -4,6 +4,6 @@
 
 
 # [[file:README.org::+BEGIN_SRC sh :tangle INSTALL.sh :comments both :shebang "#!/bin/sh"][No heading:1]]
-# apt install texlive-full
+# apt install texlive-latex-extra
 wget -q -O- https://ftp.ncbi.nih.gov/pub/HomoloGene/current/homologene.data | gzip -c > homologene.data
 # No heading:1 ends here
