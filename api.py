d8fae96e4fe00f2806bce977aca8e52817aa53b4
bar <bar@E15G3.home>
2022-11-10 15:53:58 +0300

    module path set in api.py

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import os
 from flask import Flask, send_file
 
@@ -6,8 +7,9 @@ app = Flask(__name__)
 
 @app.route("/texshade/<string:hgvsp>")
 def var2texshade(hgvsp):
-    os.system(f"./var2texshade.sh {hgvsp}")
-    return send_file(f"{hgvsp}.pdf")
+    module_path=Path("./modules/var2texshade/")
+    os.system(f"{module_path.joinpath('var2texshade.sh')} {hgvsp}")
+    return send_file(f"{module_path.joinpath(f'{hgvsp}.pdf')}")
 
 
 if __name__ == "__main__":
